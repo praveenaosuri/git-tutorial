b355575153d0ecf146ffc5413bffff2e50108b06
Praveena Osuri <praveenaosuri@gmail.com>
2025-03-18 01:27:57 +0530

    Version 1

---
+++ example.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    cout<<"Hello!\n";
+    cout<<"This is a simple C++ program.";
+
+    return 0;
+}
---
+++ src/config.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    cout<<"In case if I don't see you tomorrow\n";
+    cout<<"Good Morning Good Afternoon And Good Bye";
+
+    return 0;
+}
