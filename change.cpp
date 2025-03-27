88f5ebfc62e039a66eb36eb175d650ff8bea8b9f
Praveena Osuri <praveenaosuri@gmail.com>
2025-03-27 17:36:56 +0530

    Version 12

--- change.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-
-    cout<<"Change\n";
-    return 0;
-}
---
+++ check.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    cout<<"Checking whether it refelcts the changes in remote repo\n";
+    cout<<"Changes are reflected in local repo\n";
+    cout<<"Version 12";
+
+    return 0;
+}
